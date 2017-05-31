d6059896227e6d5dce63d561083051d881373341
nullcc <nullcc@gmail.com>
2017-05-31 18:05:57 +0800

    no msg

--- flask_api/database.py
+++ flask_api/database.py
@@ -1,18 +1,74 @@
 # -*- coding: utf-8 -*-
 
-from sqlalchemy import create_engine
+import datetime
+import functools
+from sqlalchemy import create_engine, inspect
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from flask_api import app
-from flask_api.utils.utils import to_dict
+# from Baicycle_API import application as app
+from flask_api.utils.utils import to_json_value
+from flask_api.config.config_dev import DevConfig
 
-engine = create_engine(app.config.get('SQLALCHEMY_DATABASE_URI'), encoding="utf-8", echo=True)
+db_url = DevConfig.SQLALCHEMY_DATABASE_URI
+engine = create_engine(db_url, encoding="utf-8", echo=True)
 db_session = scoped_session(sessionmaker(autocommit=True,
                                          autoflush=False,
                                          bind=engine))
 Base = declarative_base()
 Base.query = db_session.query_property()
-Base.to_dict = to_dict
+
+
+class BaseModel(object):
+
+    @classmethod
+    def select_columns(cls, results, col_names):
+        """
+        选择需要的列输出
+        :param results:
+        :param col_names:
+        :return:
+        """
+        mapper = inspect(cls)
+        if isinstance(results, list):
+            res = []
+            for element in results:
+                data = dict()
+                if col_names == "*":
+                    for attr in mapper.attrs._data:
+                        data[attr] = to_json_value(getattr(element, attr))
+                else:
+                    for col_name in col_names:
+                        data[col_name] = to_json_value(getattr(element, col_name))
+                res.append(data)
+            return res
+        else:
+            if not results:
+                return dict()
+            res = dict()
+            if col_names == "*":
+                for attr in mapper.attrs._data:
+                    res[attr] = to_json_value(getattr(results, attr))
+            else:
+                for col_name in col_names:
+                    res[col_name] = to_json_value(getattr(results, col_name))
+
+            return res
+
+    def to_dict(self):
+        """
+        把对象转换成dict
+        :param self:
+        :return:
+        """
+        res = dict()
+        for c in self.__table__.columns:
+            if isinstance(getattr(self, c.name, None), datetime.datetime):
+                res[c.name] = getattr(self, c.name, None).__str__()
+            elif isinstance(getattr(self, c.name, None), bytes):
+                res[c.name] = bool(getattr(self, c.name, None))
+            else:
+                res[c.name] = getattr(self, c.name, None)
+        return res
 
 
 def init_db():
--- flask_api/utils/utils.py
+++ flask_api/utils/utils.py
@@ -3,6 +3,7 @@
 
 import datetime
 import random
+from sqlalchemy import inspect
 
 
 def to_dict(self):
@@ -15,8 +16,34 @@ def to_dict(self):
     for c in self.__table__.columns:
         if isinstance(getattr(self, c.name, None), datetime.datetime):
             res[c.name] = getattr(self, c.name, None).__str__()
+        elif isinstance(getattr(self, c.name, None), bytes):
+            res[c.name] = bool(getattr(self, c.name, None))
         else:
             res[c.name] = getattr(self, c.name, None)
+    return res
+
+
+def to_json_value(value):
+    if isinstance(value, datetime.datetime):
+        return value.__str__()
+    elif isinstance(value, bytes):
+        return bool(value)
+    else:
+        return value
+
+
+def filter_columns(cls, results, col_names):
+    mapper = inspect(cls)
+    res = []
+    for element in results:
+        data = dict()
+        if col_names == "*":
+            for attr in mapper.attrs._data:
+                data[attr] = to_json_value(getattr(element, attr))
+        else:
+            for col_name in col_names:
+                data[col_name] = to_json_value(getattr(element, col_name))
+        res.append(data)
     return res
 
 
