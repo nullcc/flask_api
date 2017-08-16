fbdfacbbe0c8e2f05b27483deb267a4bbdb434cc
nullcc <nullcc@gmail.com>
2017-08-16 10:33:45 +0800

    update

--- celery_worker.py
+++ celery_worker.py
@@ -5,8 +5,8 @@
         celery -A celery_worker.celery --loglevel=info worker
 """
 
-from flask_api.config.config_dev import Config
+from flask_api.config.config_dev import DevConfig
 from flask_api.app import create_app
 from flask_api.extensions import celery
 
-app = create_app(Config)
+app = create_app(DevConfig)
